c7b5cd706aa76025e5fe22756e4532e9cad90a67
Oleh-Lee <rrrgo3@gmail.com>
2020-02-15 17:23:39 +0200

    done vol 2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-import tamplest from './templ.hbs'
-import menu from './menu.json'
+import tamplest from './templ.hbs';
+import menu from './menu.json';
 import './styles.css';
 import './menu.json';
-import './themes.js'
+import './themes.js';
 
 const refs = {
-    ul: document.querySelector('.menu')
+  ul: document.querySelector('.menu'),
+};
+
+function buildPostFeed(menu) {
+  const murkup = menu.map(menu => tamplest(menu)).join('');
+  refs.ul.insertAdjacentHTML('beforeend', murkup);
 }
-
-function buildPostFeed(menu){
-    const murkup = menu.map(menu => tamplest(menu)).join('');
-    refs.ul.insertAdjacentHTML('beforeend', murkup)
-}
-buildPostFeed(menu)
-
-
-console.log('eee')
-
-
+buildPostFeed(menu);
